76581953ea942e3fceee81983e8bae9a4b278274
cfcv <cfcv@cin.ufpe.br>
2019-08-13 02:13:28 +0200

    drive without training

--- autonomous_drive.py
+++ autonomous_drive.py
@@ -1,6 +1,65 @@
 import socketio
 from flask import Flask
+import eventlet
+import eventlet.wsgi
+from io import BytesIO
+import base64
+from PIL import Image
+from keras.models import load_model
+import numpy as np
+import matplotlib.pyplot as plt
+from model import pre_process
 
 server = socketio.Server()
 
 app = Flask(__name__)
+
+@server.on('connect')
+def connect(sid, environ):
+    print('connect ', sid)
+    send_control(0,0)
+
+def send_control(steering_angle, throttle):
+    server.emit("steer", 
+                data={'steering_angle': steering_angle.__str__(), 'throttle': throttle.__str__()},
+                skip_sid=True)
+
+MAX_SPEED = 25
+MIN_SPEED = 15
+
+@server.on('telemetry')
+def telemetry(sid, data):
+    print('chamou a função')
+    if data:
+        print('DATA')
+        steering_angle = float(data["steering_angle"])
+        throttle = float(data["throttle"])
+        speed = float(data["speed"])
+        
+        image = Image.open(BytesIO(base64.b64decode(data["image"])))
+
+        image = np.asarray(image)
+        image = pre_process(image)
+        image = np.array([image]) #get a 4D tensor
+        #print(image.shape)
+        #plt.imshow(image)
+        #plt.show()
+        
+        #return
+        if(speed > MAX_SPEED):
+            limit = MIN_SPEED
+        else:
+            limit = MAX_SPEED
+
+        steering_angle = float(model.predict(image, batch_size=1))
+        throttle = 1.0 - steering_angle**2 - (speed/MAX_SPEED)**2
+        print('{} {} {}'.format(steering_angle, throttle, speed))
+        
+        send_control(steering_angle, throttle)
+    else:
+        server.emit("manual", data={}, skip_sid=True)
+
+if __name__ == '__main__':
+    model = load_model("no_train.h5")
+    app = socketio.Middleware(server, app)
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
--- model.py
+++ model.py
@@ -1,17 +1,27 @@
-from keras.layers import Input, Dense, Lambda, Conv2D, Dropout
+from keras.layers import Input, Dense, Lambda, Conv2D, Dropout, Flatten
 from keras.models import Model 
+from keras.optimizers import Adam
+import numpy as np
+import cv2
 #def load_data()
 
+def pre_process(image):
+    #first the sky and the car could be remove from the image to make it smaller
+    image = image[60:130, :, :]
+    image = cv2.resize(image, (200,70), cv2.INTER_AREA)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+    return image
+
 def build_model():
-    inputs = Input(shape=(66,200,3))
+    inputs = Input(shape=(70,200,3))
 
     norm = Lambda(lambda x: x/127.5-1.0)(inputs)
 
-    convs = Conv2D(24, kernel_size=(5,5), strides=(2,2))(norm)
-    convs = Conv2D(36, kernel_size=(5,5), strides=(2,2))(convs)
-    convs = Conv2D(48, kernel_size=(5,5), strides=(2,2))(convs)
-    convs = Conv2D(64, kernel_size=(3,3))(convs)
-    convs = Conv2D(64, kernel_size=(3,3))(convs)
+    convs = Conv2D(24, kernel_size=(5,5), strides=(2,2), activation="relu")(norm)
+    convs = Conv2D(36, kernel_size=(5,5), strides=(2,2), activation="relu")(convs)
+    convs = Conv2D(48, kernel_size=(5,5), strides=(2,2), activation="relu")(convs)
+    convs = Conv2D(64, kernel_size=(3,3), activation="relu")(convs)
+    convs = Conv2D(64, kernel_size=(3,3), activation="relu")(convs)
     drop = Dropout(0.5)(convs)
     f = Flatten()(drop)
     x = Dense(100, activation='relu')(f)
@@ -20,6 +30,7 @@ def build_model():
     output = Dense(1)(x)
 
     model = Model(inputs=inputs, outputs=output)
+    model.compile(loss='mean_squared_error', optimizer=Adam())
     model.summary()
 
     return model
@@ -30,5 +41,9 @@ if __name__ == '__main__':
     #data_input = load_data()
 
     model = build_model()
-
+    model.save('no_train.h5')
     #train_model()
+    #im = np.zeros((66,200,3))
+    #a = im[60:140,:]
+    #im = pre_process(im)
+    #print(a.shape)
